bdc3311cd324899b3c7266c87dfbf19f3aa7f279
Thomas "ajnart" Camlong <thomascamlong@gmail.com>
2022-03-20 17:37:04 +0100

    Add basic Chakra UI config

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Text } from '@chakra-ui/react'
+
+const Home: NextPage = () => {
+  return (
+    <Text>
+      Hello, world!
+    </Text>
+  )
+}
+
+export default Home
