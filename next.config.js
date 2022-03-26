360664cb4ea8e0e7460a7e87aba421a2152e739c
Aj - Thomas <thomascamlong@gmail.com>
2022-03-26 15:16:03 +0100

    Update next config to add MDX

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+module.exports = {
+  // Prefer loading of ES Modules over CommonJS
+  experimental: { esmExternals: true },
+  // Support MDX files as pages:
+  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+  // Support loading `.md`, `.mdx`:
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        // The default `babel-loader` used by Next:
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          /** @type {import('@mdx-js/loader').Options} */
+          options: {/* jsxImportSource: …, otherOptions… */ }
+        }
+      ]
+    })
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-});
+    return config
+  }
+}
