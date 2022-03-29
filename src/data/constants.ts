d298f92b1a3865d92305f18432a64fcdae9ebf2b
Aj - Thomas <thomascamlong@gmail.com>
2022-03-29 18:32:11 +0200

    Add data as TS files

---
+++ src/data/constants.ts
@@ -0,0 +1,4 @@
+export const NavLinks = [
+    { label: 'hello', link: '/hello' },
+    { label: 'work', link: '/work' },
+];
---
+++ src/data/works.ts
@@ -0,0 +1,56 @@
+import { WorkCardProps } from '../components/WorkCard';
+
+export const WorkList: WorkCardProps[] = [
+  {
+    title: 'MyNetflix',
+    description: 'Docker-compose to deploy a stack of containers',
+    image: 'https://github.com/ajnart/mynetflix/blob/master/assets/mynetflix.png?raw=true',
+    link: 'https://github.com/ajnart/mynetflix',
+    tags: [
+      {
+        name: 'Docker',
+        color: 'blue',
+      },
+    ],
+  },
+  {
+    title: 'Simply Interpolated Master Password',
+    description: 'Simp is a custom password generator based on an algorithm of your choosing.',
+    image: 'https://wetry.tech/assets/img/deno/deno-logo.png',
+    link: 'https://github.com/ajnart/simp',
+    tags: [
+      {
+        name: 'TypeScript',
+        color: 'blue',
+      },
+      {
+        name: 'Deno',
+        color: 'green',
+      },
+    ],
+  },
+  {
+    title: 'Covid-19',
+    description: 'A web application to track the spread of the coronavirus.',
+    image: 'https://github.com/ajnart/mynetflix/blob/master/assets/mynetflix.png?raw=true',
+    link: 'https://github.com/ajnart/mynetflix#',
+    tags: [
+      {
+        name: 'React',
+        color: 'red',
+      },
+    ],
+  },
+  {
+    title: 'Covid-19',
+    description: 'A web application to track the spread of the coronavirus.',
+    image: 'https://github.com/ajnart/mynetflix/blob/master/assets/mynetflix.png?raw=true',
+    link: 'https://github.com/ajnart/mynetflix',
+    tags: [
+      {
+        name: 'React',
+        color: 'red',
+      },
+    ],
+  },
+];
