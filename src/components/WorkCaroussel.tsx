1e573dbbb28c40bc9d8918fa6cf270b76f513c66
Aj - Thomas <thomascamlong@gmail.com>
2022-03-30 01:57:54 +0200

    Update dependncies and redo Caroussel

--- src/components/WorkCaroussel.tsx
+++ src/components/WorkCaroussel.tsx
@@ -1,15 +1,29 @@
-import { Carousel } from 'antd';
+// import Swiper core and required modules
+import { Navigation, Pagination, A11y, Keyboard, Autoplay } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import { WorkList } from '../data/works';
 import WorkCard from './WorkCard';
 
-export default function WorkCaroussel() {
-  return (
-    <Carousel>
-      {WorkList.map((work) => (
-        <div key={work.title}>
-          <WorkCard key={work.title} {...work} />
-        </div>
-      ))}
-    </Carousel>
-  );
-}
+export default () => (
+  <Swiper
+    modules={[Navigation, Pagination, A11y, Keyboard, Autoplay]}
+    spaceBetween={50}
+    slidesPerView={1}
+    autoplay={{ delay: 10000 }}
+    keyboard
+    style={{ marginBottom: '50' }}
+    pagination={{ clickable: true }}
+  >
+    {WorkList.map((work) => (
+      <SwiperSlide>
+        <WorkCard key={work.title} {...work} />
+      </SwiperSlide>
+    ))}
+  </Swiper>
+);
