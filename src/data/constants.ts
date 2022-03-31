abbd5e8280e6cd434296bbeeb8794ff793202316
Aj - Thomas <thomascamlong@gmail.com>
2022-03-31 12:36:30 +0200

    Add CV link

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,5 +2,5 @@ export const NavLinks = [
   { label: '/', link: '/' },
   { label: 'hello', link: '/hello' },
   { label: 'work', link: '/work' },
+  { label: 'cv', url: 'https://ajnart.github.io/' },
 ];
-  
