7931fb87eddc95375bb7ea63bed9a765fa3d328a
Aj - Thomas <thomascamlong@gmail.com>
2022-03-31 11:56:47 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [
-    ],
-    rehypePlugins: [
-      
-    ],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-})
-module.exports = withMDX({
-  // Append the default value with md extensions
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+  }
+  return nextConfig
+}
