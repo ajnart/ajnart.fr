179fb6d883d3f384abfae61a9df4b6d93c87a8cc
Aj - Thomas <thomascamlong@gmail.com>
2022-03-31 12:36:59 +0200

    Next config for future Firebase endpoint

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ module.exports = async (phase, { defaultConfig }) => {
    * @type {import('next').NextConfig}
    */
   const nextConfig = {
-    /* config options here */
-  }
+    env: {
+
+    },
+  };
   return nextConfig
 }
