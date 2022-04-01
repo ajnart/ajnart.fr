425ea4dd0f45a3d51c72ae6fba0e98788e07bc25
Aj - Thomas <thomascamlong@gmail.com>
2022-04-01 03:48:05 +0200

    Fix footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -48,13 +48,7 @@ export function Footer({ links }: FooterCenteredProps) {
   ));
 
   return (
-    <Group
-      style={{ position: 'absolute', bottom: 0, left: 0, right: 0 }}
-      direction="column"
-      align="center"
-      mb={20}
-      mt={100}
-    >
+    <Group direction="column" align="center" mb={20} mt={100}>
       <Group className={classes.links}>{items}</Group>
       <Group spacing={0} position="right" noWrap>
         <ActionIcon size="lg">
