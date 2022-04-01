2d936b03cc18f8416aa874ccd22423b25eecf180
Aj - Thomas <thomascamlong@gmail.com>
2022-04-01 15:04:00 +0200

    Style layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,14 @@
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
 import { NavLinks } from '../../data/constants';
-import { CSSProperties } from 'react';
 
 export default function Layout({ children, style }: any) {
   return (
     <div>
       <Navbar links={NavLinks} />
-      <main style={{ minHeight: 'calc(70vh - 60px)' }}>{children}</main>
+      <main style={{ minHeight: 'calc(70vh - 60px)', marginRight: '20%', marginLeft: '20%' }}>
+        {children}
+      </main>
       <Footer links={[]} />
     </div>
   );
