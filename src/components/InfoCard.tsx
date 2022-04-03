e6e95af0d7873cb82d17e75b127141411246d64a
Thomas "ajnart" Camlong <thomascamlong@gmail.com>
2022-04-03 12:33:00 +0200

    Add InfoCard component

---
+++ src/components/InfoCard.tsx
@@ -0,0 +1,60 @@
+import { Box, Anchor, Avatar, Card, createStyles, Group, Image, Text } from "@mantine/core";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { Icon, Link, Location } from "tabler-icons-react";
+
+export interface InfoCardProps {
+	title: string;
+	description: string;
+	imageUrl?: string;
+	location?: string;
+	links?: { link: string; label: string, icon?: Icon }[];
+}
+
+export default function InfoCard(props: InfoCardProps) {
+	return (
+		<Box
+			sx={(theme) => ({
+				backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+				padding: theme.spacing.md,
+				width: '50%',
+			})}>
+			<Group direction="row">
+				<Group>
+
+					{/* Profile picture if it exists*/}
+					{props.imageUrl && (
+						<Avatar
+							alt="it's me"
+							src={props.imageUrl}
+							style=
+							{{
+								width: '30%',
+								height: 'auto',
+								marginBottom: 10,
+							}} />
+					)}
+					<Group direction="column" spacing={0}>
+						<Text weight={600} size="lg">
+							{props.title}
+						</Text>
+						<Text color="dimmed">{props.description}</Text>
+						{props.location &&
+							<Group spacing={0}>
+								<MdOutlineLocationOn size={25} />
+								<Text>{props.location}</Text>
+							</Group>
+						}
+					</Group>
+				</Group>
+				{props.links && props.links.map((link) => (
+					<Anchor target="_blank" href={link.link}>
+						<Group spacing={0}>
+							{link.icon ? <link.icon size={30} /> : <Link size={30} />}
+							<Text size={'lg'} >{link.label}</Text>
+						</Group>
+					</Anchor>
+				))}
+			</Group>
+		</Box >
+	);
+}
