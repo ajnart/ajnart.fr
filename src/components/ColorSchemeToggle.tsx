124001d346ffcda6e2b8c6d01d5a91ea955a7731
Thomas "ajnart" Camlong <thomascamlong@gmail.com>
2022-04-14 00:37:24 +0200

    Animate ColorSchemeToggle

--- src/components/ColorSchemeToggle.tsx
+++ src/components/ColorSchemeToggle.tsx
@@ -1,19 +1,28 @@
 import { ActionIcon, useMantineColorScheme } from '@mantine/core';
 import { Sun, MoonStars } from 'tabler-icons-react';
+import { motion } from 'framer-motion';
 
 export function ColorSchemeToggle() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
   return (
-    <ActionIcon
-      onClick={() => toggleColorScheme()}
-      size="xl"
-      sx={(theme) => ({
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        color: theme.colorScheme === 'dark' ? theme.colors.yellow[4] : theme.colors.blue[6],
-      })}
+    <motion.div
+      whileHover={{ scale: 1.2, rotate: 90 }}
+      whileTap={{
+        scale: 0.8,
+        rotate: -90,
+        borderRadius: '100%',
+      }}
     >
-      {colorScheme === 'dark' ? <Sun size={24} /> : <MoonStars size={24} />}
-    </ActionIcon>
+      <ActionIcon
+        onClick={() => toggleColorScheme()}
+        size="xl"
+        sx={(theme) => ({
+          color: theme.colorScheme === 'dark' ? theme.colors.yellow[4] : theme.colors.blue[6],
+        })}
+      >
+        {colorScheme === 'dark' ? <Sun size={24} /> : <MoonStars size={24} />}
+      </ActionIcon>
+    </motion.div>
   );
 }
