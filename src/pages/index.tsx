0e7cc045531e36b5a49b99e0e85fc9820b1d6f30
Aj - Thomas <thomascamlong@gmail.com>
2022-04-16 02:41:51 +0200

    Update index page to add animations and contact form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Title, Group, Modal, Text, Button } from '@mantine/core';
+import { Title, Group, Modal, Text } from '@mantine/core';
 import axios from 'axios';
 import * as React from 'react';
 import { VscProject } from 'react-icons/vsc';
@@ -7,8 +7,9 @@ import { links, WhoAmI } from '../data/constants';
 import ProjectsCaroussel from '../components/WorkCaroussel';
 import InfoCard from '../components/InfoCard';
 import { ExperiencesAccordion } from '../components/AccordionComponent';
-import { Mail } from 'tabler-icons-react';
 import { getCookie, setCookies } from 'cookies-next';
+import { motion } from 'framer-motion';
+import ContactForm from '../components/contact/Contact';
 
 export async function getStaticProps() {
   const commit = await axios
@@ -20,39 +21,53 @@ export async function getStaticProps() {
 
 export default function HomePage({ commit }) {
   const [modalOpen, setModalOpen] = React.useState(getCookie('modalOpen') ? false : true);
-  return (
-    <Group direction="column" grow spacing="xl">
-      <Modal
-        size={'lg'}
-        opened={modalOpen}
-        onClose={() => {
-          setModalOpen(false);
-          setCookies('modalOpen', 'true');
-        }}
-        title={<Title order={3}>🚧 This website is still under construction 🚧</Title>}
-      >
-        <Text size="xl">
-          There will be bugs, sorry about that. You can still rely on the information being
-          presented.
-        </Text>
-        <Button
-          mt="lg"
-          component="a"
-          variant="outline"
-          href="mailto:thomascamlong@gmail.com"
-          leftIcon={<Mail />}
-        >
-          Contact me
-        </Button>
-      </Modal>
-      <InfoCard {...WhoAmI} />
-      <ExperiencesAccordion />
-      <Group>
-        <VscProject size={35} />
-        <Title order={2}>Projects</Title>
+  const firstVisitModal = (
+    <Modal
+      size={'lg'}
+      opened={modalOpen}
+      onClose={() => {
+        setModalOpen(false);
+        setCookies('modalOpen', 'true');
+      }}
+      title={<Title order={3}>🚧 This website is still under construction 🚧</Title>}
+    >
+      <Text size="xl">
+        There will be bugs, sorry about that. You can still rely on the information being presented.
+      </Text>
+      <Group position="right">
+        <Text>Contact me</Text>
+        <ContactForm />
       </Group>
-      <ProjectsCaroussel />
-      <LatestCommit commit={commit} />
-    </Group>
+    </Modal>
+  );
+
+  return (
+    <>
+      <ContactForm
+        style={{
+          zIndex: 50,
+          position: 'fixed',
+          bottom: 15,
+          right: 15,
+        }}
+      />
+      {firstVisitModal}
+      <Group direction="column" grow spacing="xl">
+        <InfoCard {...WhoAmI} />
+        <ExperiencesAccordion />
+        <Group>
+          <VscProject size={35} />
+          <Title order={2}>Projects</Title>
+        </Group>
+        <ProjectsCaroussel />
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: false }}
+        >
+          <LatestCommit commit={commit} />
+        </motion.div>
+      </Group>
+    </>
   );
 }
