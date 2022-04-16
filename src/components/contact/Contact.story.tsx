331ab8ca9696064970983de72d0eaa8493997abc
Aj - Thomas <thomascamlong@gmail.com>
2022-04-16 18:35:04 +0200

    Remove code

--- src/components/contact/Contact.story.tsx
+++ src/components/contact/Contact.story.tsx
@@ -2,10 +2,6 @@
 
 import ContactForm from './Contact';
 export default {
-  /* 👇 The title prop is optional.
-   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-   * to learn how to generate automatic titles
-   */
   title: 'Contact form',
   argTypes: {
     opened: {
